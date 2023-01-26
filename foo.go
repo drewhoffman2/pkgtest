24a910d360ad4d082449e5769028b68a10caa08b
Drew Hoffman <drho7128@colorado.edu>
2023-01-26 11:40:05 -0700

    fix

--- foo.go
+++ foo.go
@@ -12,5 +12,5 @@ func Hello(name string) (string, error) {
     }*/
     // Return a greeting that embeds the name in a message.
     message := fmt.Sprintf("Hi, %v. Welcome!", name)
-    return message
+    return message, nil
 }
