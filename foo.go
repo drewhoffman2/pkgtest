c3ccef98f3a47b7919adf58d88da16e4c07e0763
Drew Hoffman <drho7128@colorado.edu>
2023-01-26 11:41:33 -0700

    fix

--- foo.go
+++ foo.go
@@ -5,12 +5,8 @@ import (
 )
 
 // Hello returns a greeting for the named person.
-func Hello(name string) (string, error) {
-    // If no name was given, return an error with a message.
-    /*if name == "" {
-        return "", errors.New("empty name")
-    }*/
+func Hello(name string) (string) {
     // Return a greeting that embeds the name in a message.
     message := fmt.Sprintf("Hi, %v. Welcome!", name)
-    return message, nil
+    return message
 }
